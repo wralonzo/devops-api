404485f5c3c6fe180ab9896a2fb26c3ae21d8759
William Alonzo <williamalonzo4519@gmail.com>
2023-10-22 20:16:37 -0600

    time expired token

--- src/main/java/edu/umg/plataforma/service/JwtServiceImpl.java
+++ src/main/java/edu/umg/plataforma/service/JwtServiceImpl.java
@@ -42,7 +42,7 @@ public class JwtServiceImpl implements JwtService {
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
                 .setIssuedAt(new Date(System.currentTimeMillis()))
-                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .setExpiration(new Date(System.currentTimeMillis() + 5000 * 120 * 24))
                 .signWith(getSigningKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
